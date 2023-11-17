b21cccfa1ef6b13fe73dabab784059ad31e0b473
amelie314 <qangela314@gmail.com>
2023-11-17 19:59:14 +0800

    Style: Update ReviewList styles for better UI presentation

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -7,28 +7,32 @@ const ReviewList = ({ reviews, currentUserId, onDelete }) => {
   return (
     <div className="space-y-4">
       {reviews.map((review) => (
-        <div key={review.id} className="flex items-start space-x-3">
+        <div
+          key={review.id}
+          className="flex items-center space-x-4 bg-black p-4 rounded-lg"
+        >
           <img
-            src={review.userPhoto}
+            src={review.userPhoto || "default-avatar.png"}
             alt={review.userName}
-            className="w-12 h-12 rounded-full object-cover"
+            className="w-10 h-10 rounded-full object-cover"
           />
           <div className="flex-1">
             <div className="flex items-center justify-between">
-              <h4 className="text-sm font-semibold text-gray-900">
+              <h4 className="text-sm font-semibold text-white">
                 {review.userName}
               </h4>
+              <span className="text-xs text-white">{/* 日期处理逻辑 */}</span>
             </div>
-            <p className="mt-1 text-sm text-white">{review.text}</p>
-            {currentUserId === review.userId && (
-              <button
-                onClick={() => onDelete(review.id)}
-                className="mt-2 text-xs text-red-600 hover:text-red-800"
-              >
-                删除
-              </button>
-            )}
+            <p className="text-sm text-white">{review.text}</p>
           </div>
+          {currentUserId === review.userId && (
+            <button
+              onClick={() => onDelete(review.id)}
+              className="ml-2 text-xs text-red-600 hover:text-red-800"
+            >
+              🗑️ 删除
+            </button>
+          )}
         </div>
       ))}
     </div>
