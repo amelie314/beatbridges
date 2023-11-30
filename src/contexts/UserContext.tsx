d4bb07b503611f156b3917798e20b687c37f24ca
amelie314 <qangela314@gmail.com>
2023-11-30 12:31:21 +0800

    Enhance UserProvider to handle userInfo updates on user state change

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,27 +1,64 @@
 /** @format */
 
-import React, { createContext, useState, useContext } from "react";
+// src/contexts/UserContext.tsx
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { auth, db } from "../firebaseConfig"; // 導入 db
+import { doc, getDoc } from "firebase/firestore"; // 導入 Firestore 的函數
+import { User } from "firebase/auth";
 
-const UserContext = createContext(null);
+// 定義額外的用戶資料類型
+interface UserInfo {
+  username?: string;
+  displayName?: string;
+  bio?: string;
+  photoURL?: string;
+}
 
-export const UserProvider = ({ children }) => {
-  const [userInfo, setUserInfo] = useState({
-    user: null,
-    username: null,
-  });
+interface UserContextType {
+  currentUser: User | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>;
+  userInfo: UserInfo | null;
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo | null>>;
+}
 
-  const resetUserInfo = () => {
-    setUserInfo({
-      user: null,
-      username: null,
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setCurrentUser(user);
+      if (user) {
+        // 從 Firestore 獲取用戶資訊
+        const userDocRef = doc(db, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          setUserInfo(userDoc.data() as UserInfo);
+        } else {
+          setUserInfo(null);
+        }
+      } else {
+        setUserInfo(null);
+      }
     });
-  };
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ userInfo, setUserInfo, resetUserInfo }}>
+    <UserContext.Provider
+      value={{ currentUser, setCurrentUser, userInfo, setUserInfo }}
+    >
       {children}
     </UserContext.Provider>
   );
 };
 
-export const useUserContext = () => useContext(UserContext);
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+  return context;
+};
