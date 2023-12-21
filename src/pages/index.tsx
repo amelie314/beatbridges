93b22d5e60c0974ba5589b824addc0d20d142dc5
amelie314 <qangela314@gmail.com>
2023-12-22 01:22:53 +0800

    🐛 fix: Adjust background image to fit screen on mobile in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,27 +75,19 @@ const LandingPage = () => {
           <title>Taiwan Concert Venues Map</title>
           <meta name="description" content="Welcome to Taiwan" />
         </Head>
-        {/* 設置背景圖片 */}
-        <div className="absolute w-full h-full bg-center bg-cover bg-no-repeat z-0">
-          <Image
-            src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYW7fHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Background Image"
-            fill
-            style={{ objectFit: "cover" }}
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-30 z-10"></div>
-        </div>
+        <div className="absolute w-full h-full bg-[url('https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3')] bg-cover bg-center bg-no-repeat"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-30 z-10"></div>
 
         {/* 歡迎訊息部分 */}
         {/* 欢迎信息部分 */}
-        <div className="pt-32 pl-16 md:pl-16 sm:pl-4 text-tertiary-color z-20 relative">
+        <div className="pt-24 pl-16 md:pl-16 sm:pl-4 text-tertiary-color z-20 relative">
           <p className="text-[60px] font-bold leading-[0.8]">Discover</p>
-          <p className="text-[38px] mt-2">Taiwan's Concert Venues</p>
+          <p className="text-[35px] mt-2">Taiwan's Concert Venues</p>
         </div>
-        <div className="pt-60 pr-8 text-right text-white z-20 relative">
+        <div className="pt-56 pr-8 text-right text-white z-20 relative">
           <p className="text-[60px] font-bold leading-[0.8]">Share </p>
-          <p className="text-[38px] mt-2">your experiences, </p>
-          <p className="text-[38px] mt-2">
+          <p className="text-[35px] mt-2">your experiences, </p>
+          <p className="text-[35px] mt-2">
             and build friendships through reviews.
           </p>
         </div>
