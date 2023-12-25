5153c99b0633036eb8924babb542f1b57d52d6bc
amelie314 <qangela314@gmail.com>
2023-12-25 19:54:41 +0800

    fix: update next.config.js to correctly configure image domains

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@
  * @type {import('next').NextConfig}
  */
 
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com", "images.unsplash.com"],
   },
+  // 在這裡添加其他配置
 };
+
+module.exports = nextConfig;
