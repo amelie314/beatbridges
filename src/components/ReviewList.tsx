0163d4fd128cc79368c516a00e40715d5babf605
amelie314 <qangela314@gmail.com>
2023-12-27 15:22:08 +0800

    feat: 🌟 Add edit functionality to ReviewList.tsx

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -5,8 +5,16 @@ import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFolder, faTrash, faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFolder,
+  faTrash,
+  faHeart,
+  faPenToSquare,
+  faTimes,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import Image from "next/image";
 
 const ReviewList = ({
   reviews,
@@ -14,8 +22,11 @@ const ReviewList = ({
   onDelete,
   onToggleFavorite,
   onLike,
+  onEdit,
 }) => {
   const [userDetails, setUserDetails] = useState({});
+  const [editingId, setEditingId] = useState(null); // 正在編輯評論的ID
+  const [editingText, setEditingText] = useState(""); // 編輯中的文本
 
   useEffect(() => {
     const loadUserDetails = async () => {
@@ -36,6 +47,24 @@ const ReviewList = ({
     loadUserDetails();
   }, [reviews]);
 
+  // 編輯按鈕的點擊事件處理函數
+  const handleEditClick = (review) => {
+    setEditingId(review.id);
+    setEditingText(review.text);
+  };
+
+  const handleUpdateReview = async (event, reviewId) => {
+    event.preventDefault();
+    await onEdit(reviewId, editingText);
+    setEditingId(null);
+    setEditingText("");
+  };
+
+  const cancelEdit = () => {
+    setEditingId(null);
+    setEditingText("");
+  };
+
   return (
     <div>
       {reviews.map((review, index) => (
@@ -50,11 +79,20 @@ const ReviewList = ({
           <div className="flex items-center">
             <div className="flex-shrink-0 mr-1">
               <Link href={`/profile/${userDetails[review.userId]?.userName}`}>
-                <img
+                {/* <img
                   src={userDetails[review.userId]?.photoURL}
                   alt={userDetails[review.userId]?.userName}
                   className="w-12 h-12 rounded-full object-cover glow"
-                />
+                /> */}
+                <div className="relative rounded-full w-[56px] h-[56px] overflow-hidden ">
+                  <Image
+                    src={userDetails[review.userId]?.photoURL}
+                    alt={userDetails[review.userId]?.userName}
+                    className="min-w-full min-h-full glow"
+                    fill
+                    objectFit="cover"
+                  />
+                </div>
               </Link>
             </div>
             <div className="ml-4 flex-1 min-w-0">
@@ -76,9 +114,52 @@ const ReviewList = ({
                 <p className="text-sm truncate text-gray-300">
                   {review.performanceName}
                 </p>
-                <p className="text-sm text-gray-300 mt-1 break-words">
-                  {review.text}
-                </p>
+
+                {editingId === review.id ? (
+                  <div className="mt-2">
+                    {editingId === review.id ? (
+                      <form onSubmit={(e) => handleUpdateReview(e, review.id)}>
+                        <div className="relative">
+                          <textarea
+                            value={editingText}
+                            onChange={(e) => setEditingText(e.target.value)}
+                            className="w-full mt-2 text-sm p-4 pr-16 pt-4 border border-gray-300 rounded text-gray-700"
+                          />
+                          <div className="absolute top-4 right-4 flex space-x-1">
+                            <button
+                              type="submit"
+                              className="p-1 text-white bg-blue-500 hover:bg-blue-700 rounded text-xs"
+                            >
+                              <FontAwesomeIcon
+                                icon={faCheck}
+                                className="text-xs"
+                              />{" "}
+                              {/* Update Icon */}
+                            </button>
+                            <button
+                              onClick={cancelEdit}
+                              className="p-1 text-white bg-red-500 hover:bg-red-700 rounded text-xs"
+                            >
+                              <FontAwesomeIcon
+                                icon={faTimes}
+                                className="text-xs"
+                              />{" "}
+                              {/* Cancel Icon */}
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    ) : (
+                      <p className="text-sm text-gray-300 mt-1 break-words">
+                        {review.text}
+                      </p>
+                    )}
+                  </div>
+                ) : (
+                  <p className="text-sm text-gray-300 mt-1 break-words">
+                    {review.text}
+                  </p>
+                )}
               </div>
             </div>
           </div>
@@ -105,6 +186,15 @@ const ReviewList = ({
                   }`}
                 />
               </button>
+              {/* Edit Button */}
+              {currentUserId === review.userId && (
+                <button onClick={() => handleEditClick(review)}>
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    className="text-xs text-gray-500"
+                  />
+                </button>
+              )}
               {/* Trash Button */}
               {currentUserId === review.userId && (
                 <button onClick={() => onDelete(review.id)}>
